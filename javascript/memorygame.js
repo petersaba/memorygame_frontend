7c73588fcbffff3bf9ce9b5d14aa3faeae16876c
Peter Saba <petersaba001@gmail.com>
2022-09-16 22:38:35 +0300

    added comments and function to remove cards or reset them after choosing

--- javascript/memorygame.js
+++ javascript/memorygame.js
@@ -1,6 +1,10 @@
 const images = ['java.png', 'javascript.png', 'python.ico']
 let imagesOrder = assignOrder()
+const revealedImages = new Array()
+const revealedIds = new Array()
 
+
+// generating numbers randomly between 0 and 6 but making an array of length 6 having only unique values
 function randomNumbers(){
     const randomArr = new Array()
     let random
@@ -13,6 +17,7 @@ function randomNumbers(){
     return randomArr
 }
 
+// assign the images to the random values inside the generated array so they can be shuffled after each play
 function assignOrder(){
     const imagesOrder = new Array()
     const randomOrder = randomNumbers()
@@ -24,11 +29,37 @@ function assignOrder(){
     return imagesOrder
 }
 
+// checking for needed conditions before revealing any image
 function reveal(event){
-    let clickedElement = event.target
-    if(clickedElement.nodeName == "DIV"){
-        clickedElement = clickedElement.firstChild
+    if (revealedImages.length < 2){
+        let clickedElement = event.target
+        if(clickedElement.nodeName == "DIV"){
+            clickedElement = clickedElement.firstChild
+        }
+        const imageId = parseInt(clickedElement.id.slice(-1))
+        clickedElement.src = "images/" + imagesOrder[imageId]
+        revealedImages.push(imagesOrder[imageId])
+        revealedIds.push(imageId)
+        if(revealedImages.length == 2){
+            setTimeout(checkResult, 500)
+        }
     }
-    const imageId = parseInt(clickedElement.id.slice(-1))
-    clickedElement.src = "images/" + imagesOrder[imageId]
+}
+
+// restoring question logo if choices are wrong and making divs hidden if its right
+function checkResult(){
+    if (revealedImages[0] == revealedImages[1]){
+        document.getElementById("div" + revealedIds[0]).style.visibility = "hidden"
+        document.getElementById("div" + revealedIds[1]).style.visibility = "hidden"
+    }else{
+        document.getElementById("img" + revealedIds[0]).src = "images/question.png"
+        document.getElementById("img" + revealedIds[1]).src = "images/question.png"
+    }
+    resetArrays()
+}
+
+// resetting the arrays while keeping them as const
+function resetArrays(){
+    revealedImages.splice(0, 2)
+    revealedIds.splice(0, 2)
 }
